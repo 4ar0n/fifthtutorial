ac002dca9a26a2855a5dd6acc0d1b0b6bbb89872
SS Lee <leesiusing@gmail.com>
2020-11-14 00:32:13 +0800

    Aaron

--- homework.py
+++ homework.py
@@ -15,12 +15,46 @@ vol_D=[10000,9200,6600,7200,8400,8600,6200,9000,9800,9400,]
 vol_E=[6000,7000,9600,8800,10000,8800,9200,7000,7000,9800,]
 
 1. get the average of the above stocks.
-2. give a list of average of the 3-day moving average (MA)
-3. trade when spot volume and price is X% higher than the 3-day  MA
+2. give the 3-day moving average (MA)
+3. purchase when spot volume and price is X% higher than the 3-day  MA and sell when its Y% lower than the 3 day MA
+
+4. You have $1,000,000 in your investment account. Write a program using the strategy 3. (but only for price) above in calculate the profit for the trade strategy for each of the stock. quantity for each purchase is 1000 shares and you cannot short sell.
+
+5. find the average , sd, max, min of all stocks
+
 
 Q2
 
 Write a programme for mortage with:
 input: 1.loan amount, , 2.annual interest rate, 3.monthly installment number
 output: repayment schedules
+
+Q3. Write a maths game for your kids:
+- 10 arithmatic problems for each game.
+- level 1 : 1 digits + - problem
+- level 2 : 2 digits + - problem
+- level 3 : 3 digits + - problem
+- level 4 : 2 digits + - x problem
+
+features: 
+1. if the answer is wrong, u need to prompt them to get them right before going to the next question. (hint: u need "while loop" )
+2. report the total mark
+3. hint: random()
+
+Q4.
+a.Write an number guess game:
+computer random choose a number and two opponenets need to above hitting the number. and showing range:1-100
+if one hits the number, he/she lost, and the game is over,the opponet won.
+however,if one give a number other than that, computer will shorten the range until that number.e.g.
+
+>>>Kason, pleaes give a number between 1-100: 
+>>>Kason: 80
+>>>Christabel, pleaes give a number between 1-80: 
+>>>Christabel: 60
+>>>Kason, pleaes give a number between 60-80
+>>>Kason: 63
+>>>Christabel, pleaes give a number between 60-73: 
+>>>Christabel: 61
+>>> BANG!THe number is 61!  Christabel, you lost!
+>>> Do you want to start a new game? (y/n)
 
