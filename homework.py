43ad51b63e2a0ad97c6d918a594b42a3268595ee
SS Lee <leesiusing@gmail.com>
2020-11-13 20:42:52 +0800

    Aaron

---
+++ homework.py
@@ -0,0 +1,26 @@
+Homework:
+
+Q1
+price_A=[99,104,88,114,112,90,113,84,103,107,]
+price_B=[108,89,117,111,119,111,85,82,81,111,]
+price_C=[104,95,88,91,116,106,90,80,96,110,]
+price_D=[91,119,116,111,101,87,112,107,81,113,]
+price_E=[106,104,85,94,91,100,83,111,114,102,]
+
+
+vol_A=[7000,8600,8800,8400,6600,9600,6400,6400,9000,10000,]
+vol_B=[9800,9200,9600,6200,8000,6800,9200,9600,7200,8000,]
+vol_C=[10000,7200,8400,7400,7400,8800,10000,9200,8600,6200,]
+vol_D=[10000,9200,6600,7200,8400,8600,6200,9000,9800,9400,]
+vol_E=[6000,7000,9600,8800,10000,8800,9200,7000,7000,9800,]
+
+1. get the average of the above stocks.
+2. give a list of average of the 3-day moving average (MA)
+3. trade when spot volume and price is X% higher than the 3-day  MA
+
+Q2
+
+Write a programme for mortage with:
+input: 1.loan amount, , 2.annual interest rate, 3.monthly installment number
+output: repayment schedules
+
