9e1729d6462f4914fa6f8626dd64a9e3a64bc37f
SS Lee <leesiusing@gmail.com>
2020-11-20 19:07:16 +0800

    Aaron

---
+++ lesson2.0_data.py
@@ -0,0 +1,7 @@
+person_db = [
+                ["Name","Sex","Age","Strength"],
+                ["Aaron",False,28,["Basketball","100m","Teaching Competition"]],
+                ["Twiggy",True,29,["Mahjong","Nothing at all"]],
+                ["Billy",False,31,["Scam","etoro"]],
+                ["Jason",False,33,["Butterfly Stroke", "1+1-you"]],
+            ]
---
+++ run.py
@@ -0,0 +1,15 @@
+namelist = ["Karl","Jason","Billy"]
+print (namelist)
+namelist.append("Aaron")
+print (namelist)
+
+namelist.pop(1)
+print (namelist)
+
+namelist.insert(0,"Jason")
+print (namelist)
+
+namelist.remove("Billy")
+print (namelist)
+
+# print (name)
