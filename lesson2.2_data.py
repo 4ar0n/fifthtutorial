dc5be61627d53da90a179e96b2eb671dd89d478b
SS Lee <leesiusing@gmail.com>
2020-11-20 17:11:02 +0800

    Aaron

---
+++ lesson2.2_data.py
@@ -0,0 +1,13 @@
+dict1 = {}
+dict1[101] = "apple"
+dict1[102] = "banana"
+
+dict2 = {999:"macOS" , 998:"CentOS" , 997:"Ubuntu" , 911:"Fedora" , 69:"Windows" , } 
+
+person_db = {
+            "Aaron":{ "Sex":False, "Age":28 , "Strength":["Basketball","100m","Teaching Competition"]},
+            "Twiggy":{ "Sex":True, "Age":29 , "Strength":["Mahjong","Nothing at all"]},
+            "Billy":{ "Sex":False, "Age":31 , "Strength":["Scam","etoro"] },
+            "Jason":{ "Sex":False, "Age":33 , "Strength":["Butterfly Stroke", "1+1-you" ]},
+
+        }
