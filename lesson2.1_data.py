1dfa7993cf0a607d49040009bb51bb1349989bf8
SS Lee <leesiusing@gmail.com>
2020-11-20 20:07:55 +0800

    Aaron

--- lesson2.1_data.py
+++ lesson2.1_data.py
@@ -1,5 +1,5 @@
 person_mapping_table = [[1,"Aaron"],[2, "Vincent"],[3,"Sir Ying"],[4,"Jason"],[5,"Billy"]]
-good_mapping_table = [45,"banana"],[46,"apple"],[47,"melon"],[48,"orange"]
+good_mapping_table = [[45,"banana"],[46,"apple"],[47,"melon"],[48,"orange"]]
 price_mapping_table = [[45,4],[46,6],[47,20],[48,3]]
 
 data=[[datetime(2020, 1, 6), 3, 45],
