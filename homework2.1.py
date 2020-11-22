b3528175cb24890b9aa6274ccb8311a6f24ea6e8
SS Lee <leesiusing@gmail.com>
2020-11-23 00:33:39 +0800

    Aaron

---
+++ homework2.1.py
@@ -0,0 +1,4 @@
+1.import csv FX_XAUUSD_intraday_60.csv
+2.write a program trade according to the x% of y-days moving averages :buy or square
+3.find an optimum value of x and y to maximize the profit. ( more about the logic than the actual result.)
+4.output the csv with the ma and decision made.
