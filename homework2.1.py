4681acb00d2dc450b21129e8763b82978fe39a94
SS Lee <leesiusing@gmail.com>
2020-11-23 10:30:36 +0800

    Aaron

--- homework2.1.py
+++ homework2.1.py
@@ -2,3 +2,4 @@
 2.write a program trade according to the x% of y-days moving averages :buy or square
 3.find an optimum value of x and y to maximize the profit. ( more about the logic than the actual result.)
 4.output the csv with the ma and decision made.
+5.use mat lab to plot a the candlestick graph
